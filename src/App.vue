d99dea868d17671bbfcbd53aac444e4ca765ae9f
davi38 <davi4alves@gmail.com>
2022-09-14 22:58:55 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <input type="text" v-model="search" />
+    <div v-for="post in filteredPosts" v-bind:key="post.id">
+      <PostView :post="post"></PostView>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostView from './components/PostView.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostView,
+  },
+  data() {
+    return {
+      search: '',
+      posts: [
+        {
+          id: 1,
+          title: 'Post 1',
+          body: 'Vue.js is cool'
+        },
+        {
+          id: 2,
+          title: 'Post 2',
+          body: 'Javascript is cool'
+        },
+        {
+          id: 3,
+          title: 'Post 3',
+          body: 'React is cool'
+        },
+      ]
+    }
+  },
+  computed: {
+    filteredPosts(){
+      return this.posts.filter(post => post.body.toLowerCase().includes(this.search.toLowerCase()))
+    }
   }
 }
+
 </script>
 
 <style>

67bbe6227a24a1aba0fe65c967e1b982cb025b17
davi38 <davi4alves@gmail.com>
2022-09-15 00:31:59 -0300

    feat: relevance implemented

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <input type="text" v-model="search" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    <input type="text" v-model="search" v-bind:onchange="(e)=>fetchArtigos(e.target.value)" />
+    <code> artigos: {{articles}}</code>
+    <div>
+      <input @change="(e)=> handleCheck(e.target.checked)" type="checkbox" id="maisRelevantes" name="maisRelevantes">
+      <label for="maisRelevantes">Mais Relevantes</label>
+    </div>
     <div v-for="post in filteredPosts" v-bind:key="post.id">
       <PostView :post="post"></PostView>
+    </div>
+    <div class='pages' v-for="n in pages" v-bind:key="n">
+      <a href='#' @click="pageChange(n)" >{{n}}</a>
     </div>
   </div>
 </template>
 
 <script>
-import PostView from './components/PostView.vue';
-
+import PostView from "./components/PostView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     PostView,
   },
   data() {
     return {
-      search: '',
-      posts: [
-        {
-          id: 1,
-          title: 'Post 1',
-          body: 'Vue.js is cool'
-        },
-        {
-          id: 2,
-          title: 'Post 2',
-          body: 'Javascript is cool'
-        },
-        {
-          id: 3,
-          title: 'Post 3',
-          body: 'React is cool'
-        },
-      ]
-    }
+      search: "calabazas",
+      posts: [],
+      pages: 0,
+      articles: 0,
+      maisRelevantes: false,
+    };
   },
   computed: {
-    filteredPosts(){
-      return this.posts.filter(post => post.body.toLowerCase().includes(this.search.toLowerCase()))
-    }
-  }
-}
+    filteredPosts() {
+      return this.posts;
+    },
+  },
+  methods: {
+    //mais relevante checado ou não
+    handleCheck(e){
+      this.maisRelevantes = (e);
+      this.pageChange(this.page)
+    },
+    // if no state para selecionar por mais relevantes
+    pageChange(page){
+      this.maisRelevantes ? (
+        fetch(
+          `https://api.beta.mejorconsalud.com/wp-json/mc/v2/posts?search=${this.search}&page=${page}&orderby=relevance`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            this.posts = res.data;
+            this.pages = res.pages;
+            this.articles = res.size;
+            console.log('mais relevantes')
+          })
+          .catch((err) => console.log(err))
+      ) : (
+        fetch(
+          `https://api.beta.mejorconsalud.com/wp-json/mc/v2/posts?search=${this.search}&page=${page}`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            this.posts = res.data;
+            this.pages = res.pages;
+            this.articles = res.size;
+            console.log('No relevantes')
+          })
+          .catch((err) => console.log(err))
+      );
+    },
 
+    fetchArtigos(search) {
+      fetch(
+        `https://api.beta.mejorconsalud.com/wp-json/mc/v2/posts?search=${search}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          this.posts = res.data;
+          this.pages = res.pages;
+          this.articles = res.size;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    this.fetchArtigos(this.search= 'calabazas');
+  },
+};
 </script>
 
 <style>
@@ -56,5 +101,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.pages{
+  margin-top: 20px;
+  padding: 3px;
+  display: inline-block;
+}
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>

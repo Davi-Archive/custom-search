4bdccbed50139228c2a0b87bf7fcfb64eaaa3bea
davi38 <davi4alves@gmail.com>
2022-09-15 18:40:43 -0300

    fix: long load because wrong primitive type

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import BlogPost from '../views/BlogPost'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import BlogPost from "../views/BlogPost.vue";
 
 const routes = [
   {
@@ -19,14 +19,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("../views/AboutView.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;

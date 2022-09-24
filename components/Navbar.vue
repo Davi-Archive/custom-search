439a711923c6e89a598579f1023c993f4b4fdf55
davi38 <davi4alves@gmail.com>
2022-09-24 15:24:59 -0300

    feat: github added to navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,7 +3,7 @@
   <nav class="w-full py-4 bg-blue-800 shadow">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
 
-      <nav>
+      <nav class="w-full">
         <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
           <li>
             <NuxtLink to="/">Home page</NuxtLink>
@@ -16,6 +16,9 @@
           <!-- <li><nuxt-link to="/about" class="button--green">
             About
           </nuxt-link></li> -->
+          <li>
+            <a href="https://github.com/davi38/custom-search" target="_blank"><button class="button-15" role="button">Github</button></a>
+          </li>
         </ul>
       </nav>
 
@@ -54,3 +57,51 @@ export default {
   name: 'Navbar',
 }
 </script>
+<style>
+/* CSS */
+    .button-15 {
+      background-image: linear-gradient(#42A1EC, #0070C9);
+      border: 1px solid #0077CC;
+      border-radius: 4px;
+      box-sizing: border-box;
+      color: #FFFFFF;
+      cursor: pointer;
+      direction: ltr;
+      display: block;
+      font-family: "SF Pro Text", "SF Pro Icons", "AOS Icons", "Helvetica Neue", Helvetica, Arial, sans-serif;
+      font-size: 17px;
+      font-weight: 400;
+      letter-spacing: -.022em;
+      line-height: 1.47059;
+      min-width: 30px;
+      overflow: visible;
+      padding: 4px 15px;
+      text-align: center;
+      user-select: none;
+      -webkit-user-select: none;
+      touch-action: manipulation;
+      white-space: nowrap;
+    }
+
+    .button-15:disabled {
+      cursor: default;
+      opacity: .3;
+    }
+
+    .button-15:hover {
+      background-image: linear-gradient(#51A9EE, #147BCD);
+      border-color: #1482D0;
+      text-decoration: none;
+    }
+
+    .button-15:active {
+      background-image: linear-gradient(#3D94D9, #0067B9);
+      border-color: #006DBC;
+      outline: none;
+    }
+
+    .button-15:focus {
+      box-shadow: rgba(131, 192, 253, 0.5) 0 0 0 3px;
+      outline: none;
+    }
+</style>
